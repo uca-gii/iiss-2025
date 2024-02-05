f8803a1e1857cdacbaeb4efcde58f0a96b9965f7
Juanma Dodero <dodero@users.noreply.github.com>
2024-02-05 11:49:38 +0100

    pdf build ammended

--- marp/pdf-build.sh
+++ marp/pdf-build.sh
@@ -1,2 +1,2 @@
-marp --allow-local-files --config-file ./marp/marp-engine.js --pdf $1.md -o pdf/$1.pdf --pdf-outlines --pdf-outlines.pages=false --pdf-outlines.headings=true
-
+marp --allow-local-files --config-file ./marp/marp-engine.js --pdf --html $1 -o ./pdf/$1.pdf --pdf-outlines --pdf-outlines.pages=false --pdf-outlines.headings=true
+open ./pdf/$1.pdf
